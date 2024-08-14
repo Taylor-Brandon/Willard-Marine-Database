1bef98136c39870a5bdf264adb9ec2ebd6bdb244
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-14 15:17:40 -0400

    set port to port 3001

--- server/server.js
+++ server/server.js
@@ -3,3 +3,5 @@ const { ApolloServer } = require("apollo-server-express");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
