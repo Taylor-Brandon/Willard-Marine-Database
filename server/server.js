12a1e3ac25917bae85adcfed23e3398e714d1264
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-14 15:22:58 -0400

    created new instance of apollo server and set to use resolvers and typeDefs

--- server/server.js
+++ server/server.js
@@ -5,3 +5,8 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
