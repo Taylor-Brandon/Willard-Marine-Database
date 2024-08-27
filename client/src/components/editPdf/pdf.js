0c520c8ff87f4502715181560a2e6e9ff5727129
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-27 17:06:20 -0400

    renamed pdf folder and added imports for pdf list

---
+++ client/src/components/editPdf/pdf.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_PDFS } from "../../utils/queries";
+import { REMOVE_PDF } from "../../utils/mutations";
+import '../../styles/style.css';
