e68b206ff94831ed90a92390481fdf5c02adef56
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-28 14:19:14 -0400

    styled change ship page

--- client/src/pages/changeShip.js
+++ client/src/pages/changeShip.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_SHIPS } from "../utils/queries";
-import { REMOVE_SHIP, UPDATE_SHIP } from '../utils/mutations';
+import { UPDATE_SHIP } from '../utils/mutations';
 import "../styles/style.css";
 
 export default function ChangeShip() {
@@ -13,14 +14,6 @@ export default function ChangeShip() {
     const [updateShip] = useMutation(UPDATE_SHIP, {
         onCompleted: (data) => {
             window.alert("Change Successful");
-        },
-        onError: (error) => {
-            window.alert(error.message)
-        }
-    });
-    const [removeShip] = useMutation(REMOVE_SHIP, {
-        onCompleted: (data) => {
-            window.alert("Ship Removed Successfully");
         },
         onError: (error) => {
             window.alert(error.message)
@@ -82,18 +75,16 @@ export default function ChangeShip() {
         e.preventDefault();
         updateShip({ variables: { shipId, ...formData } });
     };
-
-    const handleRemoveShip = () => {
-        if (window.confirm("Are you sure you want to remove this ship?")) {
-            removeShip({ variables: { shipId } });
-        }
-    };
     
     return (
         <div>
+            <Link id="changeShipHome-link" to="/home"><i className="bi bi-house"></i></Link>
+            <div id='shipInfo-section'>
+            <div id='shipInfo-card' className='card'>
             {ship && (
                 <div>
-                    <h1>{ship.shipName} {ship.model}</h1>
+                    <h1 id='ship-name'>{ship.shipName} {ship.model}</h1>
+                    <div id="ship-text">
                     <p>HIN: {ship.HIN}</p>
                     <p>HRN: {ship.HRN}</p>
                     <p>Contact Number: {ship.contactNumber}</p>
@@ -107,142 +98,160 @@ export default function ChangeShip() {
                     <p>POC Email: {ship.POCEmail}</p>
                     <p>POC Phone Number: {ship.POCPhoneNumber}</p>
                     <p>ID: {ship._id}</p>
-                    <button onClick={handleRemoveShip}>Remove Ship</button>
+                    </div>
                 </div>
             )}
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Ship Name
+            </div>
+            </div>
+            <div id="shipForm-section">
+            <div id='shipForm-card' className='card'>
+            <form className='form' onSubmit={handleSubmit}>
+            <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="shipName"
                         value={formData.shipName}
                         onChange={handleChange}
                     />
-                    </label>
-
-                    <label>
-                    Model
+                    <label htmlFor='shipName' className='form-label'>Ship Name</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="model"
                         value={formData.model}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    HIN
+                    <label htmlFor='model' className='form-label'>Model</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="HIN"
                         value={formData.HIN}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    HRN
+                    <label htmlFor='HIN' className='form-label'>HIN</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="HRN"
                         value={formData.HRN}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    Contact Number
+                    <label htmlFor='HRN' className='form-label'>HRN</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="contactNumber"
                         value={formData.contactNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    Sponson Serial Number
+                    <label htmlFor='contactNumber' className='form-label'>Contact Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="sponsonSerialNumber"
                         value={formData.sponsonSerialNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    Fuel Tank Serial Number
+                    <label htmlFor='sponsonSerialNumber' className='form-label'>Sponson Serial Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="fuelTankSerialNumber"
                         value={formData.fuelTankSerialNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    SRB Serial Number
+                    <label htmlFor='fuelTankSerialNumber' className='form-label'>Fuel Tank Serial Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="SRBSerialNumber"
                         value={formData.SRBSerialNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    ZAPR 356C2BVMX Hook Serial Number
+                    <label htmlFor='SRBSerialNumber' className='form-label'>SRB Serial Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="ZAPR356C2BVMXHookSerialNumber"
                         value={formData.ZAPR356C2BVMXHookSerialNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    Engine Make/Model
+                    <label htmlFor='ZAPR356C2BVMXHookSerialNumber' className='form-label'>ZAPR 356C2BVMX Hook Serial Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="engineMakeModel"
                         value={formData.engineMakeModel}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    Engine Serial Number
+                    <label htmlFor='engineMakeModel' className='form-label'>Engine Make and Model</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="engineSerialNumber"
                         value={formData.engineSerialNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    POC Name
+                    <label htmlFor='engineSerialNumbber' className='form-label'>Engine Serial Number</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="POCName"
                         value={formData.POCName}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    POC Email
+                    <label htmlFor='POCName' className='form-label'>POC Name</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="POCEmail"
                         value={formData.POCEmail}
                         onChange={handleChange}
                     />
-                </label>
-                <label>
-                    POC Phone Number
+                    <label htmlFor='POCEmail' className='form-label'>POC Email</label>
+                    </div>
+                    <div className='form-floating ms-3 me-3'>
                     <input
+                    className='form-control'
                         type="text"
                         name="POCPhoneNumber"
                         value={formData.POCPhoneNumber}
                         onChange={handleChange}
                     />
-                </label>
-                <button type="submit" disabled={loading}>Update Ship</button>
+                    <label htmlFor='POCPhoneNumber' className='form-label'>POC Phone Number</label>
+                    </div>
+                <button id='editShip-btn' className='btn' type="submit" disabled={loading}>Update User</button>
 {error && <p>Error: {error.message}</p>}
 </form>
 </div>
-
+</div>
+</div>
     );
 };
 
