31ed303132ad95bd45c5517f31fc64ae2b8b57c2
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-29 10:46:04 -0400

    added search component for ship and added links for profile, logout, and edit ship

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Search from '../components/search/search';
 
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <nav className='nav'>
+                <ul>
+                    <li><Link id='logout-link' to="/login">Logout</Link></li>
+                    <li><Link id='profile-link' to="/profile">Profile</Link></li>
+                    <li><Link id='editShip-link' to="/editShip">Edit Ship</Link></li>
+                </ul>
+            </nav>
+            <div>
+            <Search />
+            </div>
         </div>
     );
 }
