25f6d1e2e7c5623aeb25bd090e49be6ff71ac23e
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-09-11 11:11:56 -0400

    tested to see local storage sign up

--- client/src/pages/sign.js
+++ client/src/pages/sign.js
@@ -38,7 +38,7 @@ export default function Sign() {
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-
+      console.log(formState);
         try {
             const {data} = await addUser({
                 variables: { ...formState},
