6233c3245b7a58ecfdb605bc6eacdd3d467aef88
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-09-13 11:49:26 -0400

    added functionality to backend using multer and the front end with axios to allow for addition of pdf and file view

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
+const cors = require("cors");
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -12,11 +16,38 @@ const server = new ApolloServer({
   });
 
 const app = express();
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use("/uploads", express.static("uploads"));
 
+
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');  
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + file.originalname);  
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload-files", upload.single("file"), async(req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  console.log(req.file);
+  res.json({ file: req.file }); 
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
